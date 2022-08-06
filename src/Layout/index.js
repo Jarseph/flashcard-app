39201f97bd61e18c03ff33370fed81fe5d119417
Joseph Spencer <104227087+Jarseph@users.noreply.github.com>
2022-08-06 15:45:46 -0700

    Add files via upload

---
+++ src/Layout/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Header from "./Header";
+import NotFound from "./NotFound";
+import Home from "../Components/Home";
+import CreateDeck from "../Components/createDeck";
+import Deck from "../Components/deck";
+import Study from "../Components/study";
+import EditDeck from "../Components/editDeck";
+import AddCard from "../Components/addCard";
+import EditCard from "../Components/editCard";
+
+
+function Layout() {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/decks/new">
+                        <CreateDeck />
+                    </Route>
+                    <Route exact path="/decks/:deckId">
+                        <Deck />
+                    </Route>
+                    <Route path="/decks/:deckId/study">
+                        <Study />
+                    </Route>
+                    <Route path="/decks/:deckId/edit">
+                        <EditDeck />
+                    </Route>
+                    <Route path="/decks/:deckId/cards/new">
+                        <AddCard />
+                    </Route>
+                    <Route path="/decks/:deckId/cards/:cardId/edit">
+                        <EditCard />
+                    </Route>
+                    <NotFound />
+                </Switch>
+            </div>
+        </div>
+    );
+}
+
+export default Layout;
+
